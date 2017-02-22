30cda846c4fdf021991408518dc6be3297067fd2
Mathias <manee12@student.sdu.dk>
2017-02-23 00:33:38 +0100

    Slurp policy file with some error handling

--- parser.pl
+++ parser.pl
@@ -15,7 +15,16 @@ class TestActions {
    }
 }
 
-my $actions = TestActions.new;
-my $match = TestGrammar.parse('Policy:SSH', :$actions);
-say $match;         # ｢40｣ 
-say $match.made;    # 42 
+
+
+
+my $policy_content =  try slurp("ssh.policy");
+if ($!) {
+     note "Unable to open and read file";
+}
+print "cool nok\n"
+#die("end");
+#my $actions = TestActions.new;
+#my $match = TestGrammar.parse($policy_content, :$actions);
+#say $match;         # ｢40｣ 
+#say $match.made;    # 42 
